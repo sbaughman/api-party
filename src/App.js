5f1468a977a5f638ff01f3f2ccfdfd465db29b51
Seth Baughman <sbaughman1@me.com>
2017-05-08 09:27:15 -0400

    Add react router and set up navlinks

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function App (props) {
+  return (
+    <p>To get started, click on one of the links above</p>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -2,8 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom'
+
+import Github from './Github';
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <div className="App">
+      <div className="App-header">
+        <h1>API Party</h1>
+        <h3>Get <i>all</i> the API data</h3>
+        <ul className="nav-links">
+          <li>
+            <NavLink to={'/github'}>Github API</NavLink>
+          </li>
+        </ul>
+      </div>
+      <Switch>
+        <Route exact path='/' component={App} />
+        <Route path='/github' component={Github} />
+      </Switch>
+    </div>
+  </Router>,
   document.getElementById('root')
 );
