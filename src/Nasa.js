6bd533d0c5677ce3b89b5491faa680bfd745d010
Seth Baughman <sbaughman1@me.com>
2017-05-09 12:57:57 -0400

    Get and display basic info from NASA API

--- src/Nasa.js
+++ src/Nasa.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react'
 
+import './Nasa.css'
+
 class Nasa extends Component {
   constructor(props) {
     super(props)
+    this.state ={
+      roverName: '',
+      imageUrl: '',
+      camera: '',
+      earthDate: '',
+    }
+  }
+
+  componentWillMount() {
+    fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-05-30&camera=fhaz&api_key=DEMO_KEY')
+      .then(res => res.json())
+      .then(data => this.setState({
+        roverName: data.photos[0].rover.name,
+        imageUrl: data.photos[0].img_src,
+        earthDate: data.photos[0].earth_date
+      }))
+      .catch(err => console.warn(err))
   }
 
   render() {
     return (
       <div className="nasa">
-        Nasa page!
+        <img className="nasa-logo" src="https://www.nasa.gov/sites/default/files/images/nasaLogo-570x450.png" alt="nasa" />
+
+        <div className="rover-info">
+          <h2>Mars Rover Name: {this.state.roverName}</h2>
+          <h3>Earth Date: {this.state.earthDate}</h3>
+          <img src={this.state.imageUrl} />
+        </div>
       </div>
     )
   }
